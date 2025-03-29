a5c47593ba850d8984b3632a78e273b79458bd99
FarhanBeacon <farhanahmedbeacon@gmail.com>
2025-03-29 17:25:59 +0600

    final commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Fab TV",
+        short_name: "Fab TV",
+        description: "Stream your favorite shows and movies online.",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/icons/favicon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/public/icons/favicon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
