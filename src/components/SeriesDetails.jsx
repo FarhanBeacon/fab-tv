1caab32b966e88f40b7240e3b986299e98d97677
FarhanBeacon <farhanahmedbeacon@gmail.com>
2025-06-20 00:13:06 +0600

    fixed some adjustment

--- src/components/SeriesDetails.jsx
+++ src/components/SeriesDetails.jsx
@@ -5,8 +5,8 @@ import EpisodeIcon from "./EpisodeIcon";
 const SeriesDetails = () => {
   const { episodesData } = useLoaderData();
   return (
-    <div>
-      <h1 className="text-2xl font-bold mb-4">Episodes</h1>
+    <div className="p-2 md:p-4">
+      <h1 className="text-2xl md:text-3xl font-bold font-rancho mb-4">Watch All The Episodes Here</h1>
       <div className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-6 gap-4">
         {episodesData.map((episode, index) => (
           <EpisodeIcon
